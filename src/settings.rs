254e2e504fd8b4913b04f043d9b673ae01e3160e
Félix Saparelli <felix@passcod.name>
2019-10-14 21:14:50 +1300

    Enable case-insensitivity and full unicode whitespace command splitting

--- src/settings.rs
+++ src/settings.rs
@@ -43,7 +43,16 @@ impl Discord {
     }
 
     pub fn framework(&self) -> StandardFramework {
-        let mut fw = StandardFramework::new().configure(|c| c.prefix(&self.prefix));
+        let mut fw = StandardFramework::new().configure(|c| {
+            c.delimiters(vec![
+                "\u{0009}", "\u{0020}", "\u{00A0}", "\u{1680}", "\u{2000}", "\u{2001}", "\u{2002}",
+                "\u{2003}", "\u{2004}", "\u{2005}", "\u{2006}", "\u{2007}", "\u{2008}", "\u{2009}",
+                "\u{200A}", "\u{202F}", "\u{205F}", "\u{3000}",
+            ])
+            .prefix(&self.prefix)
+            .no_dm_prefix(true)
+            .case_insensitivity(true)
+        });
 
         if self.commands.is_empty() {
             info!("Loading all commands: {:?}", crate::commands::NAMES);
