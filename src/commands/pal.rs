5008e458f73045040ddd2aa4af33a18e9831e18b
Félix Saparelli <felix@passcod.name>
2019-11-23 21:07:21 +1300

    Cheap !pal alias – cannot be bothered to figure out how to do it properly

---
+++ src/commands/pal.rs
@@ -0,0 +1,26 @@
+use crate::{get_help, Locale};
+use serenity::client::Context;
+use serenity::framework::standard::{
+    macros::{command, group},
+    Args, CommandResult,
+};
+use serenity::model::channel::Message;
+
+group!({
+    name: "pal",
+    commands: [pal],
+});
+
+#[command]
+fn pal(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    use crate::palindromic::next_palindromic;
+    get_help!(on_empty, "palindrome", ctx, msg, args);
+
+    if let Ok(n) = args.single::<usize>() {
+        let np = next_palindromic(n);
+        msg.channel_id.say(&ctx.http, format!("{} ({} + {})", np, n, np - n))?;
+        Ok(())
+    } else {
+        super::help(ctx, msg, "palindrome")
+    }
+}
