7f4ea2c465074ccf8040a862c1cb423ac132baf8
Félix Saparelli <felix@passcod.name>
2020-02-13 17:45:14 +1300

    Dawn of a new day

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use futures::StreamExt;
+use dawn::{
+    gateway::shard::{Config, Event, Shard},
+    http::Client as HttpClient,
+};
+use std::{
+    env,
+    error::Error,
+    sync::Arc,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let token = env::var("DISCORD_TOKEN")?;
+
+    let http = Arc::new(HttpClient::new(&token));
+
+    let config = Config::builder(&token).build();
+    let shard = Shard::new(config).await?;
+    let mut events = shard.events().await;
+
+    while let Some(event) = events.next().await {
+        tokio::spawn(handler(http.clone(), event));
+    }
+
+    Ok(())
+}
+
+async fn handler(http: Arc<HttpClient>, event: Event) {
+    if let Err(err) = handle_event(http, event).await {
+        eprintln!("{:?}", err);
+    }
+}
+
+async fn handle_event(http: Arc<HttpClient>, event: Event) -> Result<(), Box<dyn Error>> {
+    match event {
+        Event::ShardConnected(connected) => {
+            println!("Connected on shard {}", connected.shard_id);
+        },
+        Event::MessageCreate(msg) => {
+            if msg.content == "!!!ping" {
+                http.create_message(msg.channel_id).content("Pong!").await?;
+            }
+        },
+        _ => {},
+    }
+
+    Ok(())
+}
+
