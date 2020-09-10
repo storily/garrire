4d2554ba6a58f6e9f401c9a9ac551c4bae25d2e1
Félix Saparelli <felix@passcod.name>
2020-09-10 23:11:32 +1200

    Support aliases, and force lowercase on commands before any matching

--- app/index.php
+++ app/index.php
@@ -10,16 +10,38 @@ function error_dump($arg): void
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 
+const ALIASES = [
+  'die' => 'roll',
+  'dice' => 'roll',
+];
+
+$segs = array_filter(explode('/', strtolower($path)));
+$ksegs = array_keys($segs);
+
 $attempts = [];
-$segs = array_filter(explode('/', $path));
-foreach (array_keys($segs) as $i) {
+foreach ($ksegs as $i) {
   $parts = array_slice($segs, 0, $i);
-  $attempts[] = implode('\\', array_map(fn ($seg) => ucfirst(strtolower($seg)), $parts));
+  $attempts[] = implode('\\', array_map(fn ($seg) => ucfirst($seg), $parts));
 }
 
 $attempts = array_reverse($attempts);
 
-// TODO: navigation-style resolving (aliases), rewriting, and restricting
+// Run through the segments, each time replacing one of them with the aliasing.
+foreach ($ksegs as $k) {
+  $subbed_segs = array_map(function ($j) use (&$segs) {
+    $sub = $segs[$j];
+    return ALIASES[$sub] ?? $sub;
+  }, $ksegs);
+
+  $sub_attempts = [];
+  foreach ($ksegs as $i) {
+    $parts = array_slice($subbed_segs, 0, $i);
+    $sub_attempts[] = implode('\\', array_map(fn ($seg) => ucfirst($seg), $parts));
+  }
+
+  $attempts = array_merge($attempts, array_reverse($sub_attempts));
+}
+
 foreach ($attempts as $attempt) {
   $controller = '\\App\\Controllers\\' . $attempt;
   if (class_exists($controller)) break;
