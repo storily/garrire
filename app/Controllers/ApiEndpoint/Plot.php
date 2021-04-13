04f5cb8d053db51ba908c9d0ccc54b1990aa3f68
Lauren Jenkinson <lauren@iris.ac.nz>
2021-04-13 19:12:53 +1200

    API: Get a single plot
    This renames the old `Plot` controller to `PlotRandom` (and adds a new
    `ApiEndpoint` entry for that as `/plot/random`), and makes the `Plot`
    controller take an `id` query parameter to return the plot with that ID.

--- app/Controllers/ApiEndpoint/Plot.php
+++ app/Controllers/ApiEndpoint/Plot.php
@@ -7,28 +7,17 @@ class Plot extends \Controllers\ApiController
 {
 	public function get(): void
 	{
-		$q = \Models\Plot::query();
+		$id = intval($_GET['id']) ?? null;
+		if ($id === null) throw new \Exceptions\End(404);
 
-		$theme = $_GET['theme'] ?? null;
-		if ($theme !== null) {
-			$theme = strtolower(trim($theme));
-			$q = $q->where('theme', 'LIKE', "%{$theme}%");
-		}
+		$plot = \Models\Plot::query()->where('id', '=', $id)->first();
+		if ($plot === null) throw new \Exceptions\End(404);
 
-		// number of plots: minimum 1, maximum 5, default 1
-		$count = max(1, min(5, intval($_GET['n']) ?? 1));
-		$plots = $q->inRandomOrder()->take($count)->get();
-
-		$result = [];
-		foreach ($plots as $plot) {
-			$result[] = [
-				"id" => $plot->id,
-				"text" => $plot->text,
-				"author" => $plot->author,
-				"theme" => $plot->theme,
-			];
-		}
-
-		$this->reply($result);
+		$this->reply([
+			"id" => $plot->id,
+			"text" => $plot->text,
+			"author" => $plot->author,
+			"theme" => $plot->theme,
+		]);
 	}
 }
